6aa83062646e29583e69ecbec9338068c9e3e67e
kevlarr <echo1@dvnl.net>
2020-03-02 21:27:11 -0500

    Add revisions dir if not present

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ pub mod executor;
 
 use crate::executor::{Executor, PostgresExecutor};
 use serde::{Deserialize};
-use std::{env, fs, path::{Path}};
+use std::{env, fs, io::prelude::*, path::{Path}};
 
 
 const CONF: &str = "jrny.toml";
@@ -74,25 +74,58 @@ pub fn connect() -> Jrny<impl Executor> {
 }
 
 pub fn start(path: &str) -> Result<(), String> {
-    println!("Setting up project at {}", path);
-    let dirpath = Path::new(path);
+    let dir_path = Path::new(path);
 
-    if dirpath.exists() {
-        if !dirpath.is_dir() {
-            return Err(format!("{} is not a directory", path));
-        }
-    } else {
-        // does dir need to explicitly be created?
-        //match fs.create_dir
+    if !dir_path.exists() {
+        fs::create_dir(&dir_path)
+            .map_err(|e| e.to_string())?;
+    } else if !dir_path.is_dir() {
+        return Err(format!("{} is not a directory", path));
     }
 
-    let confpath = dirpath.join(CONF);
-    let confpath = confpath.as_path();
+    let rev_path = dir_path.join("revisions");
+    let rev_path = rev_path.as_path();
 
+    if !rev_path.exists() {
+        // TODO remove this if later fails
+        fs::create_dir(&rev_path)
+            .map_err(|e| e.to_string())?;
+    } else if !is_empty_dir(&rev_path) {
+        return Err(format!("{} is not an empty directory", rev_path.to_str().unwrap()));
+    }
 
-    if confpath.exists() {
+    let conf_path = dir_path.join(CONF);
+    let conf_path = conf_path.as_path();
+
+    if conf_path.exists() {
         return Err(format!("{} already exists in given directory", CONF));
     }
 
+    fs::File::create(&conf_path)
+        .map_err(|e| e.to_string())?
+        .write_all(conf_template().as_bytes())
+        .map_err(|e| e.to_string())?;
+
+    println!("Setting up project at {}", path);
     Ok(())
+}
+
+fn conf_template() -> &'static str {
+r#"# jrny.toml
+
+[app]
+executor = "postgres"
+schema = "public"
+table = "jrny_revisions"
+
+[connection]
+host = "localhost"
+port = 5432
+name = "dbname"
+user = "dbrole"
+"#
+}
+
+fn is_empty_dir(p: &Path) -> bool {
+    p.is_dir() && p.read_dir().unwrap().next().is_none()
 }
