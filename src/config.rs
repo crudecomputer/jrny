326bee0f6a66ffe8a802fd87d260a94f9e209a27
kevlarr <echo1@dvnl.net>
2020-12-04 16:15:59 -0500

    Set up URL string connection strategy

--- src/config.rs
+++ src/config.rs
@@ -1,7 +1,47 @@
 use serde::Deserialize;
-use std::fs;
+use std::{env, fs};
 
 use crate::paths::ProjectPaths;
+
+/// Strategy for locating connection details.
+/// Currently only supports whole URL-style string but it could be extended to support
+/// loading config files, using separate ENV vars for host, etc.
+#[derive(Clone, Debug, Deserialize)]
+#[serde(tag = "type")]
+#[serde(rename_all = "kebab-case")]
+enum ConnectionStrategy {
+
+    #[serde(rename_all = "kebab-case")]
+    EnvUrlString { var_name: String },
+}
+
+#[derive(Clone, Debug)]
+pub struct Settings {
+    pub connection: ConnectionSettings,
+    pub table: TableSettings,
+}
+
+#[derive(Clone, Debug, Deserialize)]
+pub struct ConnectionSettings {
+    pub database_url: String,
+}
+
+#[derive(Clone, Debug, Deserialize)]
+pub struct TableSettings {
+    pub schema: String,
+    pub name: String,
+}
+
+#[derive(Clone, Debug, Deserialize)]
+struct TomlConnectionSettings {
+    pub strategy: ConnectionStrategy,
+}
+
+#[derive(Clone, Debug, Deserialize)]
+struct TomlSettings {
+    pub connection: TomlConnectionSettings,
+    pub table: TableSettings,
+}
 
 pub struct Config {
     pub paths: ProjectPaths,
@@ -13,32 +53,25 @@ impl Config {
         let paths = ProjectPaths::from_conf_path(conf_path_name)?;
 
         let contents = fs::read_to_string(&paths.conf)
-            .unwrap_or_else(|_| panic!("Could not open {}", paths.conf.display()));
+            .unwrap_or_else(|e| panic!("Could not open {}: {}", paths.conf.display(), e.to_string()));
 
-        let settings: Settings = toml::from_str(&contents)
-            .unwrap_or_else(|_| panic!("Could not open {}", paths.conf.display()));
+        let toml_settings: TomlSettings = toml::from_str(&contents)
+            .unwrap_or_else(|e| panic!("Could not open {}: {}", paths.conf.display(), e.to_string()));
+
+        let settings = Settings {
+            connection: ConnectionSettings {
+                database_url: url_from_toml(&toml_settings.connection)?,
+            },
+            table: toml_settings.table,
+        };
 
         Ok(Self { paths, settings })
     }
 }
 
-#[derive(Clone, Debug, Deserialize)]
-pub struct Settings {
-    pub connection: ConnectionSettings,
-    pub table: TableSettings,
-}
-
-#[derive(Clone, Debug, Deserialize)]
-pub struct ConnectionSettings {
-    pub host: String,
-    pub name: String,
-    pub port: u16,
-    pub user: String,
-    //pub pass: Option<String>
-}
-
-#[derive(Clone, Debug, Deserialize)]
-pub struct TableSettings {
-    pub schema: String,
-    pub name: String,
+fn url_from_toml(conn_settings: &TomlConnectionSettings) -> Result<String, String> {
+    Ok(match &conn_settings.strategy {
+        ConnectionStrategy::EnvUrlString { var_name } =>
+            env::var(var_name).map_err(|e| format!("{}: {}", e, var_name))?,
+    })
 }
--- src/lib.rs
+++ src/lib.rs
@@ -17,12 +17,19 @@ const CONF: &str = "jrny.toml";
 const CONF_TEMPLATE: &[u8] = r#"# jrny config
 
 [connection]
-host = "localhost"
-port = 5432
-name = "dbname"
-user = "dbrole"
+# Specifies how `jrny` will connect to the target database. Available strategies are:
+#
+#   env-url-string:  Use a single connection string of any supported format, the value
+#                    of which is read from an environment variable whose name is specified
+#                    by `var-name`
+#
+# NOTE: Only this single strategy is supported currently, but this is likely to be
+# extended in the future.
+#
+strategy = { type = "env-url-string", var-name = "JRNY_DATABASE_URL" }
 
 [table]
+# Specifies which schema and table `jrny` will use to track revision history.
 schema = "public"
 name = "jrny_revision"
 "#
