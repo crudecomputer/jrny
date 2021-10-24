6d091493eac8fb5df2cb85f4b2c3ac5bcf770b05
kevlarr <6306496+kevlarr@users.noreply.github.com>
2021-10-23 22:02:12 -0400

    remove templates from lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,3 @@
-use chrono::{DateTime, Local, Utc};
-use log::{info, warn};
-
 pub mod commands;
 
 mod client;
@@ -13,65 +10,17 @@ mod revisions;
 pub use error::Error;
 pub use logger::Logger;
 
-
+// Crate result type
 pub type Result<T> = std::result::Result<T, error::Error>;
 
+/// The default name of the config file
 pub const CONF: &str = "jrny.toml";
-const CONF_TEMPLATE: &str = r#"# jrny config
 
-# Project-level configuration options that should not change across environments
-# or contain any sensitive information.
-#
-# This file MUST BE INCLUDED in version control.
-
-[project]
-# The directory in which to store revisions.
-#
-# This folder can be freely renamed or moved at any point, as long as
-# the revisions within do not themselves change.
-revisions_directory = "revisions"
-
-[table]
-# Specifies which schema and table `jrny` will use to track revision history.
-#
-# These can freely be changed for new projects. To update these for existing projects
-# with revisions already executed, you would need to first manually create the new table
-# and then copy all existing revision records from the old table into the new one prior
-# to running any commands with `jrny`. Otherwise, `jrny` will attempt to run all again.
-schema = "public"
-name = "jrny_revision"
-"#;
-
+/// The default name of the environment file
 pub const ENV: &str = "jrny-env.toml";
-const ENV_TEMPLATE: &str = r#"# jrny environment
 
-# Environment-specific configuration options, including secrets such as database
-# authentication. Runtime command flags will take precedence over any values provided.
-#
-# This file MUST BE EXCULUDED from version control.
-
-[database]
-url = ""
-"#;
-
+/// The default name of the example environment file
 pub const ENV_EX: &str = "jrny-env.example.toml";
-const ENV_EX_TEMPLATE: &str = r#"# jrny environment EXAMPLE FILE
-
-# This is an example file specifying optional environment-specific to include within
-# a `jrny-env.toml` file. If that file is not present, `jrny` will require
-# that necessary secrets are passed in via command flags.
-#
-# If `jrny-secret.toml` is present, runtime command flags will take precedence
-# over any values contained within the file.
-#
-# This file SHOULD BE INCLUDED in version control.
-
-[database]
-
-# Database connection string - for permissible formats and options see:
-# https://docs.rs/postgres/0.19.1/postgres/config/struct.Config.html
-url = "postgresql://user:password@host:port/dbname"
-"#;
 
 /*
 
