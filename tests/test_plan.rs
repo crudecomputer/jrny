49ebabd4c2f3ec1a0f30b60d6386a0be378d7fad
kevlarr <6306496+kevlarr@users.noreply.github.com>
2023-04-23 08:14:07 -0400

    test ids with gap

--- tests/test_plan.rs
+++ tests/test_plan.rs
@@ -143,7 +143,7 @@ fn no_existing_revisions_and_pass_contents() {
 fn existing_revisions() {
     let title = "another.revision.file";
 
-    let dir = test_dir("plan/03-existing-revisions/revisions");
+    let dir = test_dir("plan/03a-existing-revisions/revisions");
     let cfg = Config::build()
         .revision_directory(&dir)
         .finish();
@@ -167,9 +167,28 @@ fn existing_revisions() {
 }
 
 #[test]
-fn existing_revisions_and_pass_contents() {
-}
+fn existing_revisions_with_gap_and_pass_contents() {
+    let title = "another revision file";
 
-#[test]
-fn existing_revisions_with_gap() {
+    let dir = test_dir("plan/03b-existing-revisions/revisions");
+    let cfg = Config::build()
+        .revision_directory(&dir)
+        .finish();
+
+    let start = Utc::now().timestamp();
+    sleep(Duration::from_secs(1));
+
+    let created = plan(&cfg, title, None).unwrap();
+
+    sleep(Duration::from_secs(1));
+    let end = Utc::now().timestamp();
+
+    assert_revision(
+        &created,
+        "005",
+        start..end,
+        title,
+        &DEFAULT_CONTENTS.replace("{title}", title),
+    );
+    remove_file(&created).unwrap();
 }
